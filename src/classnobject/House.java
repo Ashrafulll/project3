51728ccf14d07f428041c3709f777725cf38e20d
ashraful <ashraful.kamal10@gmail.com>
2017-10-01 14:13:06 -0400

    first add class

---
+++ src/classnobject/House.java
@@ -0,0 +1,18 @@
+package classnobject;
+
+public class House {
+	public void rooms() {
+		System.out.println("3 bed rooms");
+	}
+		public void kitchen() {
+			System.out.println("lats cook our kitchen");
+			
+		}
+		public void gestrooms() {
+			System.out.println("There Have 2 gestrooms");
+		}
+		public void studyrooms() {
+			System.out.println("There have one rooms for study");
+		}
+
+}
---
+++ src/classnobject/TestHouse.java
@@ -0,0 +1,15 @@
+package classnobject;
+
+public class TestHouse {
+
+	public static void main(String[] args) {
+		 House myHouse = new House();
+		 myHouse.rooms();
+		 myHouse.kitchen();
+		 myHouse.gestrooms();
+		 myHouse.studyrooms();
+
+		}
+
+	}
+
